604322224f774b9fe1eb27feb93768e9e038edb9
Riyaz Ali <riyaz@mergestat.com>
2022-12-28 16:28:25 +0530

    test: add check to ensure job argument passing

--- runtime/embed/embed_test.go
+++ runtime/embed/embed_test.go
@@ -3,6 +3,7 @@ package embed_test
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 	"github.com/mergestat/sqlq"
 	"github.com/mergestat/sqlq/runtime/embed"
 	"github.com/pkg/errors"
@@ -17,6 +18,26 @@ func WaitGroupNotifier(wg *sync.WaitGroup) embed.HandlerFunc {
 
 func NewNotifyTask() *sqlq.JobDescription {
 	return sqlq.NewJobDesc("notify")
+}
+
+func ArgumentTest(expected int) embed.HandlerFunc {
+	return func(_ context.Context, job *sqlq.Job) error {
+		var received int
+		if err := json.Unmarshal(job.Parameters, &received); err != nil {
+			return err
+		}
+
+		if received != expected {
+			return errors.Errorf("expected: %d\treceived: %d", expected, received)
+		}
+
+		return nil
+	}
+}
+
+func NewArgumentTest(i int) *sqlq.JobDescription {
+	var b, _ = json.Marshal(&i)
+	return sqlq.NewJobDesc("argument", sqlq.WithParameters(b))
 }
 
 func TestEmbedBasic(t *testing.T) {
@@ -43,6 +64,28 @@ func TestEmbedBasic(t *testing.T) {
 
 	// wait for the routine to execute
 	wg.Wait()
+
+	if err := worker.Shutdown(5 * time.Second); err != nil {
+		t.Fatalf("failed to shutdown worker: %v", err)
+	}
+}
+
+func TestEmbedWithParams(t *testing.T) {
+	var upstream = MustOpen(PostgresUrl)
+	defer upstream.Close()
+
+	_, _ = sqlq.Enqueue(upstream, "embed/default", NewArgumentTest(100))
+	var worker, _ = embed.NewWorker(upstream, embed.WorkerConfig{
+		Queues: []sqlq.Queue{"embed/default"},
+	})
+
+	_ = worker.Register("argument", ArgumentTest(100))
+
+	if err := worker.Start(); err != nil {
+		t.Fatalf("failed to start worker: %v", err)
+	}
+
+	time.Sleep(time.Second)
 
 	if err := worker.Shutdown(5 * time.Second); err != nil {
 		t.Fatalf("failed to shutdown worker: %v", err)
