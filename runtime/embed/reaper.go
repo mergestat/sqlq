5a545033b720835725f93c6495402baa67d1c621
Riyaz Ali <riyaz@mergestat.com>
2023-01-05 12:53:41 +0530

    chore: add code comments and apply formatting sugestions from review

--- runtime/embed/reaper.go
+++ runtime/embed/reaper.go
@@ -6,7 +6,10 @@ import (
 	"time"
 )
 
+// reaper() is the runtime service that reaps zombie processes
+// that got killed / are stuck and haven't pinged back in a while.
 func reaper(worker *Worker) (shutdown func() error) {
+	// reap wraps sqlq.Reap() and run it inside a transaction
 	var reap = func() (err error) {
 		var tx *sql.Tx
 		if tx, err = worker.db.Begin(); err != nil {
@@ -24,11 +27,13 @@ func reaper(worker *Worker) (shutdown func() error) {
 	var quit = make(chan struct{}, 1)
 	var done = func() error { close(quit); return reap() }
 
+	// we run the reaper process asynchronously, in the background,
+	// and clear up zombies every 5 seconds.
 	go func() {
 		for {
 			select {
 			case <-quit:
-				return
+				return // received signal to terminate
 			case <-time.After(5 * time.Second):
 				_ = reap()
 			}
