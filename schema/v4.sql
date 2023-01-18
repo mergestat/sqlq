d5ccc27da1e02d8da87a6c72850e9c614844e3ba
Riyaz Ali <riyaz@mergestat.com>
2023-01-18 14:37:59 +0530

    fix: multiply by 1e9 to set resolution to nanoseconds; default used by golang

--- schema/v4.sql
+++ schema/v4.sql
@@ -3,7 +3,7 @@
 -- Column keepalive_interval is the interval in seconds at which the worker
 -- is expected to send the pings. Defaults to 30 seconds.
 ALTER TABLE sqlq.jobs
-    ADD COLUMN keepalive_interval BIGINT NOT NULL DEFAULT (30);
+    ADD COLUMN keepalive_interval BIGINT NOT NULL DEFAULT (30 * 1e9);
 
 -- Column last_keepalive stores the last ping's time
 ALTER TABLE sqlq.jobs
